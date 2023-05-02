4d826ca6c0a501fdca4dc86ab21ce35639b61adc
Meph384 <jakub.kozak384@gmail.com>
2023-05-02 21:59:32 +0200

    style: initial app.component setup with grid template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './side-nav-menu/side-nav-menu.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TopNavMenuComponent } from './top-nav-menu/top-nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    TopNavMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
