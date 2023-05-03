b2a0e23eefbea98db6c13769763bc6eaf48c4943
Meph384 <jakub.kozak384@gmail.com>
2023-05-03 23:02:03 +0200

    style: desktop dashboard + remove static width/height

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NavMenuComponent } from './side-nav-menu/side-nav-menu.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TopNavMenuComponent } from './top-nav-menu/top-nav-menu.component';
 import { ProfileViewComponent } from './profile-view/profile-view.component';
+import { UpgradeToProComponent } from './upgrade-to-pro/upgrade-to-pro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     TopNavMenuComponent,
-    ProfileViewComponent
+    ProfileViewComponent,
+    UpgradeToProComponent
   ],
   imports: [
     BrowserModule,
